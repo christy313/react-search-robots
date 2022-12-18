8a86b9068604ce53ec26a7eb87932ae8e7dc6753
christy313 <christycreativity@gmail.com>
2022-12-18 13:50:52 -0500

    Replace searchField state from redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
 import "tachyons";
+import { setSearchField } from "../actions";
 
-export default function App() {
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchField,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+  };
+};
+
+function App({ searchField, onSearchChange }) {
   const [robots, setRobots] = useState([]);
-  const [searchField, setSearchField] = useState("");
+
+  // const [searchField, setSearchField] = useState("");
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -15,9 +30,9 @@ export default function App() {
       .then((users) => setRobots(users));
   }, []);
 
-  const onSearchChange = (e) => {
-    setSearchField(e.target.value);
-  };
+  // const onSearchChange = (e) => {
+  //   setSearchField(e.target.value);
+  // };
 
   const filteredRobots = robots.filter((robot) =>
     robot.name.toLowerCase().includes(searchField.toLowerCase())
@@ -39,3 +54,5 @@ export default function App() {
     );
   }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./containers/App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import "./index.css";
+import { searchRobots } from "./reducers";
 
+const store = createStore(searchRobots);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
